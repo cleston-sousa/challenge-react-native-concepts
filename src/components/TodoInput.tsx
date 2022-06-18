de1dbfb1f2f8f89ad12c6486f9c0978442455e04
Cleston Sousa <cleston.vs@gmail.com>
2022-06-18 02:38:06 -0300

    challenge resolution

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -10,7 +10,7 @@ export function TodoInput({ addTask }: TodoInputProps) {
   const [task, setTask] = useState('');
 
   function handleAddNewTask() {
-    if(task!==''){
+    if(!!task && task.trim()!=''){
       addTask(task);
     }
     setTask('');
@@ -26,6 +26,7 @@ export function TodoInput({ addTask }: TodoInputProps) {
         selectionColor="#666666"
         onChangeText={setTask}
         value={task}
+        onSubmitEditing={handleAddNewTask}
       />
       <TouchableOpacity
         testID="add-new-task-button"
